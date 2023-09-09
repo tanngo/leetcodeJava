d3bf571847cee83e7955d9435df8605ec947e24b
ngonguyen <tanngontn@gmail.com>
2023-09-09 16:21:59 -0500

    add diameter sample

--- LeetCode/src/BinaryTree/DiameterOfBinaryTree.java
+++ LeetCode/src/BinaryTree/DiameterOfBinaryTree.java
@@ -1,9 +1,35 @@
 package BinaryTree;
 //https://leetcode.com/problems/diameter-of-binary-tree/description/
 
-public class DiameterOfBinaryTree {
+ //Definition for a binary tree node.
 
+public class DiameterOfBinaryTree {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
     public static void main(String[] args) throws Exception {
-        
+
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] res = new int[1];
+        dfs(root,res);
+        return res[0];
+    }
+    public int dfs(TreeNode root, int []res){
+        if (root ==null) return -1;
+        int left = dfs(root.left, res);
+        int right = dfs(root.right,res);
+        res[0] = Math.max(res[0],left+right+2);
+        return 1+ Math.max(left,right);
     }
 }
